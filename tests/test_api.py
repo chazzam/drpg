1c0537bcfc05102b8dc607a8f6cf86a308484943
NeilC <24608830+nexuk@users.noreply.github.com>
2025-05-25 19:11:51 -0700

    Fix api tests to mock requests
    Cloudscraper uses requests under the covers, so the API tests need to mock that instead of httpx.

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,8 +3,7 @@ from functools import partial
 from unittest import TestCase, mock
 from urllib.parse import urlencode, urlparse
 
-import respx
-from httpx import Response
+import requests_mock
 
 from drpg import api
 
@@ -13,26 +12,22 @@ from .fixtures import PrepareDownloadUrlResponseFixture
 _api_url = urlparse(api.DrpgApi.API_URL)
 api_base_url = f"{_api_url.scheme}://{_api_url.hostname}"
 
-# Workaround for https://github.com/lundberg/respx/issues/277
-tmp_respx_mock = partial(respx.mock, using="httpx")
-
-
 class DrpgApiTokenTest(TestCase):
     def setUp(self):
-        self.login_url = "api/vBeta/auth_key"
+        self.login_url = f"{api_base_url}/api/vBeta/auth_key"
         self.client = api.DrpgApi("token")
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_login_valid_token(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_login_valid_token(self, mock):
         content = {"token": "some-token", "refreshToken": "123", "refreshTokenTTL": 171235}
-        respx_mock.post(self.login_url).respond(200, json=content)
+        mock.post(self.login_url, json=content)
 
         login_data = self.client.token()
         self.assertEqual(login_data, content)
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_login_invalid_token(self, respx_mock):
-        respx_mock.post(self.login_url).respond(401)
+    @requests_mock.Mocker()
+    def test_login_invalid_token(self, mock):
+        mock.post(self.login_url, status_code=401)
 
         with self.assertRaises(AttributeError):
             self.client.token()
@@ -40,32 +35,30 @@ class DrpgApiTokenTest(TestCase):
 
 class DrpgApiCustomerProductsTest(TestCase):
     def setUp(self):
-        url = "api/vBeta/order_products"
+        url = f"{api_base_url}/api/vBeta/order_products"
         self.products_page = re.compile(f"{url}\\?.+$")
         self.client = api.DrpgApi("token")
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_one_page(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_one_page(self, mock):
         page_1_products = [{"name": "First Product"}]
-        respx_mock.get(self.products_page).mock(
-            side_effect=[
-                Response(200, json=page_1_products),
-                Response(200, json=[]),
+        mock.get(self.products_page, [
+                {'json':page_1_products},
+                {'json':[]},
             ]
         )
 
         products = self.client.customer_products()
         self.assertEqual(list(products), page_1_products)
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_multiple_pages(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_multiple_pages(self, mock):
         page_1_products = [{"name": "First Product"}]
         page_2_products = [{"name": "Second Product"}]
-        respx_mock.get(self.products_page).mock(
-            side_effect=[
-                Response(200, json=page_1_products),
-                Response(200, json=page_2_products),
-                Response(200, json=[]),
+        mock.get(self.products_page, [
+                {'json':page_1_products},
+                {'json':page_2_products},
+                {'json':[]},
             ]
         )
         products = self.client.customer_products()
@@ -77,10 +70,10 @@ class DrpgApiPrepareDownloadUrlTest(TestCase):
         self.order_product_id = 123
         params = urlencode({"siteId": 10, "index": 0, "getChecksums": 0})
         self.prepare_download_url = (
-            f"/api/vBeta/order_products/{self.order_product_id}/prepare?{params}"
+            f"{api_base_url}/api/vBeta/order_products/{self.order_product_id}/prepare?{params}"
         )
         self.check_download_url = (
-            f"/api/vBeta/order_products/{self.order_product_id}/check?{params}"
+            f"{api_base_url}/api/vBeta/order_products/{self.order_product_id}/check?{params}"
         )
 
         self.response_preparing = PrepareDownloadUrlResponseFixture.preparing()
@@ -88,26 +81,25 @@ class DrpgApiPrepareDownloadUrlTest(TestCase):
 
         self.client = api.DrpgApi("token")
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_immiediate_download_url(self, respx_mock):
-        respx_mock.get(self.prepare_download_url).respond(200, json=self.response_ready)
+    @requests_mock.Mocker()
+    def test_immiediate_download_url(self, mock):
+        mock.get(self.prepare_download_url, json=self.response_ready)
 
         file_data = self.client.prepare_download_url(self.order_product_id, 0)
         self.assertEqual(file_data, self.response_ready)
 
-    @mock.patch("drpg.api.sleep")
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_wait_for_download_url(self, _, respx_mock):
-        respx_mock.get(self.prepare_download_url).respond(200, json=self.response_preparing)
-        respx_mock.get(self.check_download_url).respond(200, json=self.response_ready)
+    @requests_mock.Mocker()
+    def test_wait_for_download_url(self, mock):
+        mock.get(self.prepare_download_url, json=self.response_preparing)
+        mock.get(self.check_download_url, json=self.response_ready)
 
         file_data = self.client.prepare_download_url(self.order_product_id, 0)
         self.assertEqual(file_data, self.response_ready)
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_unsuccesful_response(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_unsuccesful_response(self, mock):
         dummy_400_response = {"message": "Invalid product id"}
-        respx_mock.get(self.prepare_download_url).respond(400, json=dummy_400_response)
+        mock.get(self.prepare_download_url, status_code=400, json=dummy_400_response)
 
         with self.assertRaises(self.client.PrepareDownloadUrlException) as cm:
             self.client.prepare_download_url(self.order_product_id, 0)
@@ -115,10 +107,10 @@ class DrpgApiPrepareDownloadUrlTest(TestCase):
             cm.exception.args, (self.client.PrepareDownloadUrlException.REQUEST_FAILED,)
         )
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_unexpected_response_with_string_message(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_unexpected_response_with_string_message(self, mock):
         dummy_unexpected_response = {"message": "Invalid product id"}
-        respx_mock.get(self.prepare_download_url).respond(200, json=dummy_unexpected_response)
+        mock.get(self.prepare_download_url, json=dummy_unexpected_response)
 
         with self.assertRaises(self.client.PrepareDownloadUrlException) as cm:
             self.client.prepare_download_url(self.order_product_id, 0)
@@ -126,10 +118,10 @@ class DrpgApiPrepareDownloadUrlTest(TestCase):
             cm.exception.args, (self.client.PrepareDownloadUrlException.UNEXPECTED_RESPONSE,)
         )
 
-    @tmp_respx_mock(base_url=api_base_url)
-    def test_unexpected_response_with_json_message(self, respx_mock):
+    @requests_mock.Mocker()
+    def test_unexpected_response_with_json_message(self, mock):
         dummy_unexpected_response = {"message": {"reason": "Invalid product id"}}
-        respx_mock.get(self.prepare_download_url).respond(200, json=dummy_unexpected_response)
+        mock.get(self.prepare_download_url, json=dummy_unexpected_response)
 
         with self.assertRaises(self.client.PrepareDownloadUrlException) as cm:
             self.client.prepare_download_url(self.order_product_id, 0)
